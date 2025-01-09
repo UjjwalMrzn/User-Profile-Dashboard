cef6a6b1f1e9cfbbfc12028d65ec616945625e61
UjjwalMrzn <ujjwal.mrzn@gmail.com>
2025-01-09 15:45:29 +0545

    display fetched data

--- script.js
+++ script.js
@@ -3,7 +3,18 @@ async function person() {
   let response = await fetch("https://jsonplaceholder.typicode.com/users");
   if(!response.ok) throw new Error("Data not available in source.");
   let data = await response.json();
-  console.log(data)
+  const container = document.getElementById("userContainer")
+  data.forEach((user) => {
+    const card = document.createElement("div");
+      card.className = "card";
+      card.innerHTML = `
+      <h3>${user.name}</h3>
+      <p>Email : ${user.email}</p>
+      <p>City : ${user.address.city}</p>
+      <p>Phone :${user.phone}</p>
+      `;
+    container.appendChild(card);
+  });
 }
 catch(error){
   console.error("NO DATA : " , error.message)
